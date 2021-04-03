bbce11ada889e6bcb7ef7fd8cc71067282d297a2
windwolf <zhoujian.ww@gmail.com>
2021-04-03 11:39:55 +0800

    增加通过jlink reset erase flash的命令

--- cmake/stm32-gcc-jlink.cmake
+++ cmake/stm32-gcc-jlink.cmake
@@ -1,28 +1,26 @@
 # name of targets
+set(ELF_TARGET ${PROJECT_NAME}.elf)
 set(BIN_TARGET ${PROJECT_NAME}.bin)
 set(HEX_TARGET ${PROJECT_NAME}.hex)
 set(MAP_TARGET ${PROJECT_NAME}.map)
 set(LSS_TARGET ${PROJECT_NAME}.lss)
 
+
+set(JLINK_EXE "C:\\Program Files (x86)\\SEGGER\\JLink\\JLINK.exe")
+
 # flash
+configure_file(${CMAKE_SOURCE_DIR}/scripts/jlink-flash-stm32.jlink ${CMAKE_CURRENT_BINARY_DIR}/jlink-flash-stm32.jlink)
 add_custom_target(jlink_flash 
-    COMMAND JLinkExe -device ${MCU_NAME} -speed 4000 -if SWD -CommanderScript ${CMAKE_SOURCE_DIR}/scripts/jlink-flash-fw-standalone.jlink
-)
+	COMMAND ${JLINK_EXE} -Device ${JLINK_DEVICE} -Speed auto -if SWD -CommanderScript ${CMAKE_CURRENT_BINARY_DIR}/jlink-flash-stm32.jlink
+	DEPENDS ${ELF_TARGET}
+	)
+
+# erase
+add_custom_target(jlink_erase 
+	COMMAND ${JLINK_EXE} -Device ${JLINK_DEVICE} -Speed auto -if SWD -CommanderScript ${CMAKE_SOURCE_DIR}/scripts/jlink-erase-stm32.jlink
+	)
 
 # reset
 add_custom_target(jlink_reset 
-    COMMAND JLinkExe -device ${MCU_NAME} -speed 4000 -if SWD -CommanderScript ${CMAKE_SOURCE_DIR}/scripts/jlink-reset-stm32.jlink
+    COMMAND ${JLINK_EXE} -Device ${JLINK_DEVICE} -Speed auto -If SWD -CommanderScript ${CMAKE_SOURCE_DIR}/scripts/jlink-reset-stm32.jlink
 )
-
-# GDB
-add_custom_target(jlink_gdb_server 
-    COMMAND JLinkGDBServer -device ${MCU_NAME} -if SWD
-)
-
-add_custom_target(jlink_gdb_debug 
-    COMMAND ${ARM_GDB_EXECUTABLE} -x ${CMAKE_SOURCE_DIR}/scripts/jlink-debug.gdbinit ${PROJECT_NAME}.elf
-)
-
-# # string should be less than 30 bytes
-# release_binary:
-# 	printf "westonrobot-$(shell git rev-parse --short HEAD)\r\n" | cat - $(PROG).bin > weston-scout-$(shell git rev-parse --short HEAD).bin
