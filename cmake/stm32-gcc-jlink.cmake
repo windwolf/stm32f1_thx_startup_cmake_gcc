afff19d8cc79885bcd2ca9b4ff3530dabb9879bc
windwolf <zhoujian.ww@gmail.com>
2021-04-09 14:30:30 +0800

    调整scripts位置

--- cmake/stm32-gcc-jlink.cmake
+++ cmake/stm32-gcc-jlink.cmake
@@ -9,7 +9,7 @@ set(LSS_TARGET ${PROJECT_NAME}.lss)
 set(JLINK_EXE "C:\\Program Files (x86)\\SEGGER\\JLink\\JLINK.exe")
 
 # flash
-configure_file(${CMAKE_SOURCE_DIR}/scripts/jlink-flash-stm32.jlink ${CMAKE_CURRENT_BINARY_DIR}/jlink-flash-stm32.jlink)
+configure_file(${CMAKE_SOURCE_DIR}/cmake/scripts/jlink-flash-stm32.jlink ${CMAKE_CURRENT_BINARY_DIR}/jlink-flash-stm32.jlink)
 add_custom_target(jlink_flash 
 	COMMAND ${JLINK_EXE} -Device ${JLINK_DEVICE} -Speed auto -if SWD -CommanderScript ${CMAKE_CURRENT_BINARY_DIR}/jlink-flash-stm32.jlink
 	DEPENDS ${ELF_TARGET}
@@ -17,10 +17,10 @@ add_custom_target(jlink_flash
 
 # erase
 add_custom_target(jlink_erase 
-	COMMAND ${JLINK_EXE} -Device ${JLINK_DEVICE} -Speed auto -if SWD -CommanderScript ${CMAKE_SOURCE_DIR}/scripts/jlink-erase-stm32.jlink
+	COMMAND ${JLINK_EXE} -Device ${JLINK_DEVICE} -Speed auto -if SWD -CommanderScript ${CMAKE_SOURCE_DIR}/cmake/scripts/jlink-erase-stm32.jlink
 	)
 
 # reset
 add_custom_target(jlink_reset 
-    COMMAND ${JLINK_EXE} -Device ${JLINK_DEVICE} -Speed auto -If SWD -CommanderScript ${CMAKE_SOURCE_DIR}/scripts/jlink-reset-stm32.jlink
+    COMMAND ${JLINK_EXE} -Device ${JLINK_DEVICE} -Speed auto -If SWD -CommanderScript ${CMAKE_SOURCE_DIR}/cmake/scripts/jlink-reset-stm32.jlink
 )
