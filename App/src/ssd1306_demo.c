cccf6bbc6a6e999b1223872cf291ccfb265d0a21
zhoujian <zhoujian.ww@gmail.com>
2022-07-07 00:23:10 +0800

    canvas:   将buffer从结构中移出来 ssd1306:   setpos考虑屏幕尺寸限制 ssd1306demo:   0.91和0.96寸两个oled调试完成

--- App/src/ssd1306_demo.c
+++ App/src/ssd1306_demo.c
@@ -7,11 +7,44 @@
 #define LOG_MODULE "SSD1306demo"
 #include "log.h"
 
-PinDevice scl;
-PinDevice sda;
-SoftI2CDevice softi2c;
-I2CDevice i2c;
+PinDevice scl0_91;
+PinDevice sda0_91;
+SoftI2CDevice softi2c0_91;
+I2CDevice i2c0_91;
+
 SSD1306 ZJ0_91in = {
+    .width = 128,
+    .height = 32,
+    .memoryMode = SSD1306_MEMORY_ADDRESSING_MODE_VERTICAL,
+    .enableChargePump = true,
+    .comInverted = true,
+    .segmentInverted = true,
+    .comLeftRightRemap = false,
+    .comAlternative = false,
+    .displayInverted = false,
+    .displayStartLine = 0,
+    .displayOffset = 0,
+    .multiplexRatio = 31,
+    .phase1period = 0x02,
+    .phase2period = 0x02,
+    .vcomhDeselectLevel = 0x40,
+    .fosc = 0x0F,
+    .clkDivide = 0x00,
+};
+
+CanvasInfo canvasInfo_ZJ0_91in = {
+    .width = 128,
+    .height = 32,
+    .direction = FONT_MEMORY_LAYOUT_DIRECTION_VERTICAL,
+    .pixelSize = PIXEL_SIZE_1BIT,
+};
+
+PinDevice scl0_96;
+PinDevice sda0_96;
+SoftI2CDevice softi2c0_96;
+I2CDevice i2c0_96;
+
+SSD1306 ZJ0_96in = {
     .width = 128,
     .height = 64,
     .memoryMode = SSD1306_MEMORY_ADDRESSING_MODE_VERTICAL,
@@ -19,7 +52,7 @@ SSD1306 ZJ0_91in = {
     .comInverted = true,
     .segmentInverted = true,
     .comLeftRightRemap = false,
-    .comAlternative = false,
+    .comAlternative = true,
     .displayInverted = false,
     .displayStartLine = 0,
     .displayOffset = 0,
@@ -31,13 +64,13 @@ SSD1306 ZJ0_91in = {
     .clkDivide = 0x00,
 };
 
-CanvasInfo canvasInfo = {
+CanvasInfo canvasInfo_ZJ0_96in = {
     .width = 128,
     .height = 64,
-    .buffer = (uint8_t *)ZJ0_91in.data_buffer,
     .direction = FONT_MEMORY_LAYOUT_DIRECTION_VERTICAL,
     .pixelSize = PIXEL_SIZE_1BIT,
 };
+
 FontDrawInfo fontDrawInfo = {
     .backColor = 0x0,
     .foreColor = 0x1,
@@ -47,13 +80,21 @@ FontDrawInfo fontDrawInfo = {
 
 static void device_init()
 {
-    pin_device_create(&scl, GPIOB, GPIO_PIN_6, PIN_DEVICE_STATUS_INVERSE_NORMAL);
-    pin_device_create(&sda, GPIOB, GPIO_PIN_7, PIN_DEVICE_STATUS_INVERSE_NORMAL);
-    softi2c.scl = &scl;
-    softi2c.sda = &sda;
-    i2c_device_create(&i2c, &softi2c);
-    ssd1306_create(&ZJ0_91in, &i2c);
+    pin_device_create(&scl0_91, GPIOB, GPIO_PIN_6, PIN_DEVICE_STATUS_INVERSE_NORMAL);
+    pin_device_create(&sda0_91, GPIOB, GPIO_PIN_7, PIN_DEVICE_STATUS_INVERSE_NORMAL);
+    softi2c0_91.scl = &scl0_91;
+    softi2c0_91.sda = &sda0_91;
+    i2c_device_create(&i2c0_91, &softi2c0_91);
+    ssd1306_create(&ZJ0_91in, &i2c0_91);
     ssd1306_init(&ZJ0_91in);
+
+    pin_device_create(&scl0_96, GPIOA, GPIO_PIN_12, PIN_DEVICE_STATUS_INVERSE_NORMAL);
+    pin_device_create(&sda0_96, GPIOA, GPIO_PIN_15, PIN_DEVICE_STATUS_INVERSE_NORMAL);
+    softi2c0_96.scl = &scl0_96;
+    softi2c0_96.sda = &sda0_96;
+    i2c_device_create(&i2c0_96, &softi2c0_96);
+    ssd1306_create(&ZJ0_96in, &i2c0_96);
+    ssd1306_init(&ZJ0_96in);
 }
 void ssd1306_demo(void)
 {
@@ -61,7 +102,24 @@ void ssd1306_demo(void)
 
     while (1)
     {
-        FONTS_FillData(&canvasInfo, 0, 0, "!!", &Font6x8_v, &fontDrawInfo);
+        FONTS_FillData(ZJ0_96in.data_buffer, &canvasInfo_ZJ0_96in, 0, 0, "0123456789ABCDEFGHIJK", &Font6x8_v, &fontDrawInfo);
+        FONTS_FillData(ZJ0_96in.data_buffer, &canvasInfo_ZJ0_96in, 0, 8, "123456789ABCDEFGHIJK0", &Font6x8_v, &fontDrawInfo);
+        FONTS_FillData(ZJ0_96in.data_buffer, &canvasInfo_ZJ0_96in, 0, 16, "23456789ABCDEFGHIJK01", &Font6x8_v, &fontDrawInfo);
+        FONTS_FillData(ZJ0_96in.data_buffer, &canvasInfo_ZJ0_96in, 0, 24, "3456789ABCDEFGHIJK012", &Font6x8_v, &fontDrawInfo);
+        FONTS_FillData(ZJ0_96in.data_buffer, &canvasInfo_ZJ0_96in, 0, 32, "456789ABCDEFGHIJK0123", &Font6x8_v, &fontDrawInfo);
+        FONTS_FillData(ZJ0_96in.data_buffer, &canvasInfo_ZJ0_96in, 0, 40, "56789ABCDEFGHIJK01234", &Font6x8_v, &fontDrawInfo);
+        FONTS_FillData(ZJ0_96in.data_buffer, &canvasInfo_ZJ0_96in, 0, 48, "6789ABCDEFGHIJK012345", &Font6x8_v, &fontDrawInfo);
+        FONTS_FillData(ZJ0_96in.data_buffer, &canvasInfo_ZJ0_96in, 0, 56, "789ABCDEFGHIJK0123456", &Font6x8_v, &fontDrawInfo);
+        ssd1306_draw(&ZJ0_96in);
+
+        FONTS_FillData(ZJ0_91in.data_buffer, &canvasInfo_ZJ0_91in, 0, 0, "0123456789ABCDEFGHIJK", &Font6x8_v, &fontDrawInfo);
+        FONTS_FillData(ZJ0_91in.data_buffer, &canvasInfo_ZJ0_91in, 0, 8, "123456789ABCDEFGHIJK0", &Font6x8_v, &fontDrawInfo);
+        FONTS_FillData(ZJ0_91in.data_buffer, &canvasInfo_ZJ0_91in, 0, 16, "23456789ABCDEFGHIJK01", &Font6x8_v, &fontDrawInfo);
+        FONTS_FillData(ZJ0_91in.data_buffer, &canvasInfo_ZJ0_91in, 0, 24, "3456789ABCDEFGHIJK012", &Font6x8_v, &fontDrawInfo);
+        FONTS_FillData(ZJ0_91in.data_buffer, &canvasInfo_ZJ0_91in, 0, 32, "456789ABCDEFGHIJK0123", &Font6x8_v, &fontDrawInfo);
+        FONTS_FillData(ZJ0_91in.data_buffer, &canvasInfo_ZJ0_91in, 0, 40, "56789ABCDEFGHIJK01234", &Font6x8_v, &fontDrawInfo);
+        FONTS_FillData(ZJ0_91in.data_buffer, &canvasInfo_ZJ0_91in, 0, 48, "6789ABCDEFGHIJK012345", &Font6x8_v, &fontDrawInfo);
+        FONTS_FillData(ZJ0_91in.data_buffer, &canvasInfo_ZJ0_91in, 0, 56, "789ABCDEFGHIJK0123456", &Font6x8_v, &fontDrawInfo);
         ssd1306_draw(&ZJ0_91in);
         LOG_I("draw finished.");
     }
--- cubemx/Core/Src/gpio.c
+++ cubemx/Core/Src/gpio.c
@@ -1,21 +1,21 @@
 /* USER CODE BEGIN Header */
 /**
-  ******************************************************************************
-  * @file    gpio.c
-  * @brief   This file provides code for the configuration
-  *          of all used GPIO pins.
-  ******************************************************************************
-  * @attention
-  *
-  * Copyright (c) 2022 STMicroelectronics.
-  * All rights reserved.
-  *
-  * This software is licensed under terms that can be found in the LICENSE file
-  * in the root directory of this software component.
-  * If no LICENSE file comes with this software, it is provided AS-IS.
-  *
-  ******************************************************************************
-  */
+ ******************************************************************************
+ * @file    gpio.c
+ * @brief   This file provides code for the configuration
+ *          of all used GPIO pins.
+ ******************************************************************************
+ * @attention
+ *
+ * Copyright (c) 2022 STMicroelectronics.
+ * All rights reserved.
+ *
+ * This software is licensed under terms that can be found in the LICENSE file
+ * in the root directory of this software component.
+ * If no LICENSE file comes with this software, it is provided AS-IS.
+ *
+ ******************************************************************************
+ */
 /* USER CODE END Header */
 
 /* Includes ------------------------------------------------------------------*/
@@ -33,12 +33,12 @@
 /* USER CODE END 1 */
 
 /** Configure pins as
-        * Analog
-        * Input
-        * Output
-        * EVENT_OUT
-        * EXTI
-*/
+ * Analog
+ * Input
+ * Output
+ * EVENT_OUT
+ * EXTI
+ */
 void MX_GPIO_Init(void)
 {
 
@@ -54,7 +54,10 @@ void MX_GPIO_Init(void)
   HAL_GPIO_WritePin(GPIOC, GPIO_PIN_13, GPIO_PIN_RESET);
 
   /*Configure GPIO pin Output Level */
-  HAL_GPIO_WritePin(GPIOB, I2C_SCL_Pin|I2C_SDA_Pin, GPIO_PIN_RESET);
+  HAL_GPIO_WritePin(GPIOB, I2C_SCL_Pin | I2C_SDA_Pin, GPIO_PIN_RESET);
+
+  /*Configure GPIO pin Output Level */
+  HAL_GPIO_WritePin(GPIOA, GPIO_PIN_12 | GPIO_PIN_15, GPIO_PIN_RESET);
 
   /*Configure GPIO pin : PC13 */
   GPIO_InitStruct.Pin = GPIO_PIN_13;
@@ -64,12 +67,17 @@ void MX_GPIO_Init(void)
   HAL_GPIO_Init(GPIOC, &GPIO_InitStruct);
 
   /*Configure GPIO pins : PBPin PBPin */
-  GPIO_InitStruct.Pin = I2C_SCL_Pin|I2C_SDA_Pin;
+  GPIO_InitStruct.Pin = I2C_SCL_Pin | I2C_SDA_Pin;
   GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
   GPIO_InitStruct.Pull = GPIO_NOPULL;
   GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
   HAL_GPIO_Init(GPIOB, &GPIO_InitStruct);
 
+  GPIO_InitStruct.Pin = GPIO_PIN_12 | GPIO_PIN_15;
+  GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
+  GPIO_InitStruct.Pull = GPIO_NOPULL;
+  GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
+  HAL_GPIO_Init(GPIOA, &GPIO_InitStruct);
 }
 
 /* USER CODE BEGIN 2 */
