fa664fd556a1b853b686a61db1f97f6e0e213345
zhoujian <zhoujian.ww@gmail.com>
2022-07-11 21:52:30 +0800

    log   支持RTT和UART的log输出,通过编译选项控制   print功能从base移到device

--- project-settings.cmake
+++ project-settings.cmake
@@ -6,18 +6,17 @@ set(PROJECT_BUILD_TYPE "DEBUG")
 # TODO: 目标MCU
 set(TARGET_MCU "STM32F103C8T6")
 
-
 option(USE_LL_LIB "Enable LL library" ON)
 option(USE_HAL_LIB "Enable HAL library" ON)
 option(USE_SYSTEM_VIEW "Enable Segger SystemView library" OFF)
-option(USE_RTT "Enable RTT logout" ON)
-
+option(USE_UART_PRINT "Enable USE_UART_PRINT" ON)
+option(USE_RTT_PRINT "Enable USE_RTT_PRINT" ON)
 
 # set(OS_PORT "threadx")
 set(OS_PORT "nortos")
+
 # set(OS_PORT "freertos")
 # set(OS_PORT_FREERTOS_MEM_MANG "heap4")
-
 
 # set(BSP_PORT "stm32h750")
 set(BSP_PORT "stm32f1xx")
